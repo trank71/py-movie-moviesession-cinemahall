19de44afe0f28c0760fb6be322741bf06fe2a574
Ambra <trank13f@gmail.com>
2026-03-28 00:46:31 +0700

    create get_movie_session func for movie_session service

--- services/movie_session.py
+++ services/movie_session.py
@@ -6,7 +6,7 @@ from db.models import MovieSession
 
 
 def create_movie_session(
-        movie_show_time: datetime.datetime,
+        movie_show_time: str,
         movie_id: int,
         cinema_hall_id: int,
 ) -> MovieSession:
@@ -15,3 +15,11 @@ def create_movie_session(
         movie_id=movie_id,
         cinema_hall_id=cinema_hall_id,
     )
+
+
+def get_movie_session(
+        session_date: str = None,
+) -> QuerySet:
+    if session_date:
+        return MovieSession.objects.filter(show_time__date=session_date)
+    return MovieSession.objects.all()
