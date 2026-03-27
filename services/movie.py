6ac48f846f0d6563e744d3897c1110e253e4b44d
Ambra <trank13f@gmail.com>
2026-03-27 11:23:54 +0700

    create create_movie service for movie model

--- services/movie.py
+++ services/movie.py
@@ -26,3 +26,21 @@ def get_movie(
 @property
 def get_movie_by_id(movie_id: int) -> Movie:
     return Movie.objects.get(id=movie_id)
+
+
+@property
+def create_movie(
+        movie_title: str,
+        movie_description: str,
+        genres_ids: list[int] = None,
+        actors_ids: list[int] = None
+) -> Movie:
+    movie = Movie.objects.create(
+        title=movie_title,
+        description=movie_description,
+    )
+    if genres_ids:
+        movie.genres.set(genres_ids)
+    if actors_ids:
+        movie.actors.set(actors_ids)
+    return movie
