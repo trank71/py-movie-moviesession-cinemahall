9d91f51c1b8f3e1b4285c3e6887561b251a96f38
Ambra <trank13f@gmail.com>
2026-03-27 11:29:51 +0700

    create create_cinema_hall func for cinemaHall service

--- services/cinema_hall.py
+++ services/cinema_hall.py
@@ -5,3 +5,13 @@ from db.models import CinemaHall
 
 def get_cinema_halls() -> QuerySet:
     return CinemaHall.objects.all()
+
+
+def create_cinema_hall(hal_name: str,
+                       hall_rows: int,
+                       hall_seats_in_row: int
+                       ) -> CinemaHall:
+    return CinemaHall.objects.create(
+        hal_name=hal_name,
+        hall_rows=hall_rows,
+        hall_seats_in_row=hall_seats_in_row)
