eb73cf1baa79ce8af89609c7781daaa7bc6dfcf2
Ambra <trank13f@gmail.com>
2026-03-28 10:55:35 +0700

    create delete_movie_session func for movie_session service

--- services/movie_session.py
+++ services/movie_session.py
@@ -46,3 +46,7 @@ def update_movie_session(
         movie_session.cinema_hall_id = cinema_hall_id
     movie_session.save()
     return movie_session
+
+
+def delete_movie_session_by_id(session_id: int) -> None:
+    MovieSession.objects.get(id=session_id).delete()
